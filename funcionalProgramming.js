940ea8b51fc7fee5588a9ecfd157a27986f49f11
Elise Flaneuse <113945437+eliseflaneuse@users.noreply.github.com>
2023-02-27 10:06:05 -0300

    Update funcionalProgramming.js

--- funcionalProgramming.js
+++ funcionalProgramming.js
@@ -33,7 +33,20 @@ Syllabus:
 5. Refactor Global Variables Out of Functions
 6.Use the map Method to Extract Data from an Array
 7. Implement map on a Prototype
-
+8. Use the filter Method to Extract Data from an Array - Array.prototype.filter()
+9. Implement the filter Method on a Prototype
+10. Return Part of an Array Using the slice Method
+11. Remove Elements from an Array Using slice Instead of splice (splice is a mutating method as slice is not)
+12. Combine Two Arrays Using the concat Method
+13. Use the reduce Method to Analyze Data
+14. Use Higher-Order Functions map, filter, or reduce to Solve a Complex Problem
+15. Sort an Array Alphabetically using the sort Method
+16. Return a Sorted Array Without Changing the Original Array
+17. Split a String into an Array Using the split Method
+18.Combine an Array into a String Using the join Method
+19. Apply Functional Programming to Convert Strings to URL Slugs
+20. Use the every Method to Check that Every Element in an Array Meets a Criteria
+21. Use the some Method to Check that Any Elements in an Array Meet a Criteria
 */
 
 // 1. Understand Functional Programming Terminology
